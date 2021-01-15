08cd8dc4bda12f5b304649eae0d38e2f37bb0276
Eric <eric1932777@gmail.com>
2021-01-15 17:02:18 -0600

    Add jobs/xuexia15

---
+++ jobs/xuexia15.py
@@ -0,0 +1,60 @@
+import json
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from jobs.example import JobTemplate
+
+# Constants
+URL = "https://www.xuexia15.cc"
+with open("./credentials/xuexia15.json") as f:
+    cred_dict = json.loads(f.read())
+USER = cred_dict["username"]
+PASS = cred_dict["password"]
+
+
+class Job(JobTemplate):
+    def __init__(self):
+        super().__init__("xuexia15")
+
+    def exec(self):
+        driver = JobTemplate.get_driver_chrome()
+
+        try:
+            # Part 1: handle log in
+            driver.set_page_load_timeout(5)  # driver.get timeout
+            # driver.set_script_timeout(5)
+            try:
+                driver.get(URL)
+            except TimeoutException:
+                pass  # just continue
+            logged_in = True
+            try:
+                driver.find_element_by_id("um")
+            except NoSuchElementException:
+                logged_in = False
+            if not logged_in:
+                user = driver.find_element_by_id("ls_username")
+                user.send_keys(USER)
+                pwd = driver.find_element_by_id("ls_password")
+                pwd.send_keys(PASS)
+                btn_login = driver.find_element_by_css_selector(
+                    "form#lsform>div:first-child>div>table>tbody>tr:nth-child(2)>td:nth-child(3)>button")
+                btn_login.click()
+                # TODO
+                driver.set_page_load_timeout(10)
+                try:
+                    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#um")))
+                finally:
+                    pass
+
+            # Part 2: click 打卡签到
+            qiandao = driver.find_elements_by_xpath("//*[contains(text(), '打卡签到')]")[0]
+            qiandao.click()
+
+            # TODO add validation
+        finally:
+            driver.quit()
