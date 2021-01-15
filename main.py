21641edb69f4c1d8a895b98dff3e94798e6f5da5
Eric <eric1932777@gmail.com>
2021-01-15 17:10:34 -0600

    Update URL path of signing in & add page "/list"

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from wrapper import Wrapper
 
@@ -21,7 +22,7 @@ def root():
     return {"Title": "Hello World!"}
 
 
-@app.get("/{item}")
+@app.get("/sign/{item}")
 async def call(item: str):
     exist = (item + ".py") in os.listdir("./jobs") and item != "example"
     if not exist:
@@ -30,3 +31,27 @@ async def call(item: str):
 
     instance = Wrapper.get_instance(item)
     return instance.call()
+
+
+@app.get("/list", response_class=HTMLResponse)
+def list_jobs():
+    html_template = """
+    <html>
+        <head>
+            <title>All Jobs</title>
+        </head>
+        <body>
+            <HTML_BODY>
+        </body>
+    </html>
+    """
+    html_body = ""
+    for x in os.listdir("./jobs"):
+        if x == "example.py" or not x.endswith(".py"):
+            continue
+        html_body += f"""
+        <div>
+            <a>{x[:-3]}</a>
+        </div>
+        """
+    return html_template.replace("<HTML_BODY>", html_body)
