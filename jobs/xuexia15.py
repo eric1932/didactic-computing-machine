fc1ced7e0a21c002f53f730dc0dd43eb8c74eb8d
Eric <eric1932777@gmail.com>
2021-01-16 01:49:22 -0600

    Add comments and an assertion; move USER & PASS

--- jobs/xuexia15.py
+++ jobs/xuexia15.py
@@ -9,11 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from jobs.example import JobTemplate
 
 # Constants
-URL = "https://www.xuexia15.cc"
-with open("./credentials/xuexia15.json") as f:
-    cred_dict = json.loads(f.read())
-USER = cred_dict["username"]
-PASS = cred_dict["password"]
+URL = "https://www.xuexia15.cc/forum.php"
 
 
 class Job(JobTemplate):
@@ -21,10 +17,16 @@ class Job(JobTemplate):
         super().__init__("xuexia15")
 
     def exec(self):
+        with open("./credentials/xuexia15.json") as f:
+            cred_dict = json.loads(f.read())
+        USER = cred_dict["username"]
+        PASS = cred_dict["password"]
+
         driver = JobTemplate.get_driver_chrome()
 
         try:
             # Part 1: handle log in
+            # don't want to wait for too long
             driver.set_page_load_timeout(5)  # driver.get timeout
             # driver.set_script_timeout(5)
             try:
@@ -44,12 +46,14 @@ class Job(JobTemplate):
                 btn_login = driver.find_element_by_css_selector(
                     "form#lsform>div:first-child>div>table>tbody>tr:nth-child(2)>td:nth-child(3)>button")
                 btn_login.click()
-                # TODO
+                # TODO simplify
                 driver.set_page_load_timeout(10)
                 try:
                     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#um")))
-                finally:
+                except Exception:
                     pass
+
+            assert driver.find_element_by_id("um")
 
             # Part 2: click 打卡签到
             qiandao = driver.find_elements_by_xpath("//*[contains(text(), '打卡签到')]")[0]
